b357839f6ba8b45433c581b464e737c8c1fe24ee
Arpit Tripathi (a04t) <arpit04tripathi@gmail.com>
2024-04-22 08:53:04 +0100

    add basename to router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/react-js">
       <div className="App">
         <header>
           <AppHeader />
