2d11ffed94b5c60699a65f703470527e457cc73a
Arpit Tripathi (a04t) <arpit04tripathi@gmail.com>
2024-04-22 12:29:44 +0100

    root files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/custom.scss';
+import './assets/css/custom.scss';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
