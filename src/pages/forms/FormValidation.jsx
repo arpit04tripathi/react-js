e8d55a6e6f4bec32ea2d88fd5009cd6fc496b8d9
Arpit Tripathi <arpit04tripathi@gmail.com>
2024-06-21 09:15:49 +0100

    page view validation form

--- src/pages/forms/FormValidation.jsx
+++ src/pages/forms/FormValidation.jsx
@@ -2,11 +2,32 @@ import AccordionItem from "../../components/AccordionItem";
 import EnrollmentForm from "../../features/forms/EnrollmentForm";
 
 const FormValidation = () => {
-
   return (
     <div id="validate-forms">
+      <header className="bg-dark text-white rounded-1 p-3 mb-3">
+        <h1>Form Validation</h1>
+        <hr />
+        <ul>
+          <li>
+            Read more about{" "}
+            <a
+              href="https://getbootstrap.com/docs/5.0/forms/validation/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Bootstrap 5 form validation
+            </a>{" "}
+            <code>no library</code>
+          </li>
+        </ul>
+      </header>
       <div className="accordion" id="accordionExample">
-        <AccordionItem itemId="Two" parentId="accordionExample" heading="Enrollment Form" show>
+        <AccordionItem
+          itemId="Two"
+          parentId="accordionExample"
+          heading="Enrollment Form"
+          show
+        >
           <EnrollmentForm />
         </AccordionItem>
       </div>
